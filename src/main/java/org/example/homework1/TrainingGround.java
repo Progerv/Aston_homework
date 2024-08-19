b499cbbd292913a38fad294f20a1b807000d0949
v.haimoev <haimoev@gmail.com>
2024-08-19 10:31:40 +0300

    Homework 1, 1 part

---
+++ src/main/java/org/example/homework1/TrainingGround.java
@@ -0,0 +1,10 @@
+package org.example.homework1;
+
+public class TrainingGround {
+    public static void main(String[] args) {
+
+        Hero hero = new Hero("Valor");
+        hero.attackEnemy();
+
+    }
+}
